c969284db30e96be01addcc629723e541a8725e2
Andy Farrell <andrew.farrell@experian.com>
2019-08-16 16:33:33 +0100

    Don't minify JS any more, it doesn't work well

--- build.sh
+++ build.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 HASH=$(date +%Y%m%d%H%M%S)
 BASE_FILENAME=page.$HASH
-JS_FILENAME=$BASE_FILENAME.min.js
+JS_FILENAME=$BASE_FILENAME.js
 CSS_FILENAME=$BASE_FILENAME.css
 JS_PATH=build/javascript
 CSS_PATH=build/css
-INPUT_JS=(
-  assets/javascript/page.js
-)
-echo Minifying these files: "${INPUT_JS[@]}"
 
 mkdir -p $JS_PATH
 mkdir -p $CSS_PATH
 rm $JS_PATH/*.js
 rm $CSS_PATH/*.css
 
-terser -m --warn "${INPUT_JS[@]}" > $JS_PATH/$JS_FILENAME &&
-  echo $JS_PATH/$JS_FILENAME generated
+cp assets/javascript/page.js $JS_PATH/$JS_FILENAME &&
+  echo $JS_PATH/$JS_FILENAME created
 
 cp assets/css/page.css $CSS_PATH/$CSS_FILENAME &&
   echo $CSS_PATH/$CSS_FILENAME created
